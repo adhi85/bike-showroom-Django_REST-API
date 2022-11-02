5e965bb48add4ab666187d57df67e4c5f2190247
adhi85 <adimuthanikatt@gmail.com>
2022-11-02 23:29:09 +0530

    added login api

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,12 @@
 
 from django.urls import path
+from knox import views as knox_views
+
 from . import views
 urlpatterns = [
-    # path('',views.home, name="home")
+    path('',views.home, name="home"),
      path('api/register/', views.RegisterAPI.as_view(), name='register'),
+      path('api/login/', views.LoginAPI.as_view(), name='login'),
+        path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+     path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
